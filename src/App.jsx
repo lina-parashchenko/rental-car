ff24ca64144cd6f630d6309930406d0041e09a03
Lina Parashchenko <blond2109@gmail.com>
2025-09-10 23:02:18 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Catalog from "./pages/Catalog";
+import CarDetails from "./pages/CarDetails";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/catalog" element={<Catalog />} />
+      <Route path="/catalog/:id" element={<CarDetails />} />
+    </Routes>
+  );
+}
+
+export default App;
